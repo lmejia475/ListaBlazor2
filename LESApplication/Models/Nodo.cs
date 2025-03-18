2056943da303ca152d1a882aa374459971def641
luis mejia <luis03mejia03@gmail.com>
2025-03-18 17:05:01 -0600

    primer commit

---
+++ LESApplication/Models/Nodo.cs
@@ -0,0 +1,25 @@
+﻿namespace LESApplication.Models
+{
+    public class Nodo
+    {
+        public string Informacion { get; set; }
+        public Nodo? Referencia { get; set; }
+
+        public Nodo()
+        {
+            Informacion = string.Empty;
+            Referencia = null;
+        }
+
+        public Nodo(string informacion)
+        {
+            Informacion = informacion;
+            Referencia = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Informacion} ==> ";
+        }
+    }
+}
---
+++ LESApplication/Services/LES.cs
@@ -0,0 +1,50 @@
+﻿using LESApplication.Models;
+
+namespace LESApplication.Services
+{
+    public class LES
+    {
+        public Nodo? PrimerNodo { get; set; }
+        public Nodo? UltimoNodo { get; set; }
+
+        public LES()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+        } 
+
+        bool EstaVacia()
+        {
+            return PrimerNodo == null;
+        }
+
+        public string AgregarNodoFinal(Nodo nuevoNodo)
+        {
+            if (EstaVacia())
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                UltimoNodo.Referencia = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            return "Nodo agregado al final de la lista";
+        }
+        public string AgregarNodoInicio(Nodo nuevoNodo)
+        {
+            if (EstaVacia())
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.Referencia = PrimerNodo;
+                PrimerNodo = nuevoNodo;
+            }
+            return "Nodo agregado al final de la lista";
+        }
+    }
+}
