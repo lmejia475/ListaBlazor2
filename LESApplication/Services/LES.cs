d16fa527912f297e20d39db2b8c1ce84305a6fcc
Jason Pivaral <jasonminerp@gmail.com>
2025-03-18 21:37:39 -0600

    agregue el buscar, agregar despues de x y antes de x

--- LESApplication/Services/LES.cs
+++ LESApplication/Services/LES.cs
@@ -11,7 +11,7 @@ namespace LESApplication.Services
         {
             PrimerNodo = null;
             UltimoNodo = null;
-        } 
+        }
 
         bool EstaVacia()
         {
@@ -45,6 +45,76 @@ namespace LESApplication.Services
                 PrimerNodo = nuevoNodo;
             }
             return "Nodo agregado al final de la lista";
+        }
+        public Nodo? BuscarNodo(string x)
+        {
+            Nodo? actual = PrimerNodo;
+
+            while (actual != null)
+            {
+                if (actual.Informacion == x)
+                {
+                    return actual;
+                }
+                actual = actual.Referencia;
+            }
+
+            Console.WriteLine("\nNodo no encontrado");
+            return null;
+        }
+
+
+        public string AgregarNodoDespuesDeX(string x, String nuevoDato)
+        {
+            Nodo? nodoX = BuscarNodo(x);
+            if (nodoX != null)
+            {
+                Nodo nuevoNodo = new Nodo(nuevoDato);
+                nuevoNodo.Referencia = nodoX.Referencia;
+                nodoX.Referencia = nuevoNodo;
+                if (nodoX == UltimoNodo)
+                {
+                    UltimoNodo = nuevoNodo;
+                }
+            }
+            else
+            {
+                return "Nodo X no encontrado";
+            }
+            return "Nodo agregado despues de x";
+        }
+        public string AgregarNodoAntesDeX(string x, String nuevoDato)
+        {
+            Nodo? nodoX = BuscarNodo(x);
+            if (nodoX != null)
+            {
+                Nodo nuevoNodo = new Nodo(nuevoDato);
+                if (nodoX == PrimerNodo)
+                {
+                    nuevoNodo.Referencia = PrimerNodo;
+                    PrimerNodo = nuevoNodo;
+                }
+                else
+                {
+                    Nodo actual = PrimerNodo;
+                    while (actual != null && actual.Referencia != nodoX)
+                    {
+                        actual = actual.Referencia;
+                    }
+                    if (actual != null)
+                    {
+                        nuevoNodo.Referencia = nodoX;
+                        actual.Referencia = nuevoNodo;
+                    }
+                }
+                return "Nodo Agregado antes de X";
+
+            }
+            else
+            {
+                return "Nodo X no encontrado";
+            }
+
         }
     }
 }
